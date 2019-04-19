18f8fd2f3101d99de5aa3a876a13eff53d581779
Kalam Shaikh <kalams@successive.com>
2019-04-19 09:08:36 +0530

    ObjectClasess

---
+++ Training/src/main/java/personDetails.java
@@ -0,0 +1,18 @@
+public class personDetails {
+
+    public  static  void main (String[]args) {
+        teamQA person1 = new teamQA();
+        System.out.println(person1.name ="kalam");
+        System.out.println(person1.gender ='M');
+        System.out.println(person1.age =22);
+        System.out.println(person1.designation = "AssociateQA");
+        System.out.println(person1.projectName = "MC");
+
+        teamQA person2 = new teamQA();
+        System.out.println(person2.name ="Naman");
+        System.out.println(person2.gender ='M');
+        System.out.println(person2.age =27);
+        System.out.println(person2.designation = "Senior QA Enginner");
+        System.out.println(person2.projectName = "MC");
+    }
+}
---
+++ Training/src/main/java/teamQA.java
@@ -0,0 +1,8 @@
+public class teamQA
+{
+    String name;
+    char gender;
+    int age;
+    String designation;
+    String projectName;
+}
